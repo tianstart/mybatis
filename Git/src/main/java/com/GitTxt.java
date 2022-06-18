4b5a514bbaa3fca325baab9aa4c90c3299cee0ac
Tian <2398026495@qq.com>
2022-06-18 11:25:23 +0800

    Master first commit

---
+++ Git/src/main/java/com/GitTxt.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @Author: Tian
+ * @CreateTime: 2022-06-18  11:10
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class GitTxt {
+    public static void main(String[] args) {
+        System.out.println("主分支第一次提交");
+    }
+}
