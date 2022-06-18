ed0b542913bae7628d9413030fe80f18f0dfb775
Tian <2398026495@qq.com>
2022-06-18 12:02:54 +0800

    hotfix first commit

--- Git/src/main/java/com/GitTxt.java
+++ Git/src/main/java/com/GitTxt.java
@@ -1,13 +1,9 @@
 package com;
 
-/**
- * @Author: Tian
- * @CreateTime: 2022-06-18  11:10
- * @Description: TODO
- * @Version: 1.0
- */
+
 public class GitTxt {
     public static void main(String[] args) {
         System.out.println("主分支第一次提交");
+        System.out.println("hot-fix第一次修改");
     }
 }
